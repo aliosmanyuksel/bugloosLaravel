1678d466af9303648c30d6041484ecebcb621f49
Ali Osman Yüksel <aliosmanyuksel@outlook.com>
2023-07-30 13:56:16 +0300

    comment lines added

--- app/Http/Controllers/api/v1/APIController.php
+++ app/Http/Controllers/api/v1/APIController.php
@@ -1,7 +1,9 @@
 <?php
 
+// Define the namespace for this class.
 namespace App\Http\Controllers\api\v1;
 
+// Import necessary classes.
 use App\Http\Controllers\Controller;
 use App\Services\APIService;
 use App\Services\ParserService;
@@ -11,15 +13,22 @@ use App\Repositories\ProductImagesRepository;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Log;
 
+/*
+ * APIController is a controller that handles API requests related to products.
+ * It uses services and repositories to handle different aspects of the data fetching, parsing, mapping, and storage processes.
+ */
 class APIController extends Controller
 {
+    // These properties hold instances of the services and repositories used by this controller.
     private APIService $apiService;
     private ParserService $parserService;
     private MapperService $mapperService;
     private ProductRepository $productRepository;
     private ProductImagesRepository $productImagesRepository;
 
-    // We inject the services and repositories we are going to use through the constructor
+    /*
+     * The constructor method is used to inject the necessary services and repositories into this controller.
+     */
     public function __construct(
         APIService              $apiService,
         ParserService           $parserService,
@@ -35,12 +44,17 @@ class APIController extends Controller
         $this->productImagesRepository = $productImagesRepository;
     }
 
-    // The fetchData function is responsible for getting the data from the external API
-    // parsing the XML to an array, mapping the array to match our database fields,
-    // and then saving the data to the database
+    /*
+     * The fetchData method is responsible for fetching, parsing, mapping, and storing product data from an external API.
+     * If successful, it returns a JSON response with a success message.
+     * If an error occurs at any point in the process, it logs the error and returns a JSON response with an error message.
+     *
+     * @return \Illuminate\Http\JsonResponse - A JSON response indicating the success or failure of the operation.
+     */
     public function fetchData(): JsonResponse
     {
         try {
+            // Fetch data and parse it.
             $url = env('API_URL');
             $configFile = storage_path(env('CONFIG_PATH'));
 
@@ -48,17 +62,15 @@ class APIController extends Controller
             $contentType = $this->apiService->getContentType($url);
             $parsedData = $this->parserService->parse($data, $contentType);
 
+            // Map and store data.
             foreach ($parsedData['products'] as $product) {
                 $mappedData = $this->mapperService->map($product, $configFile);
 
-                // Separate the images data from the rest of the product data
+                // Separate images data and save product and images data.
                 $imagesData = $mappedData['image'];
                 unset($mappedData['image']);
-
-                // Save the product data
                 $product = $this->productRepository->create((array)$mappedData);
 
-                // Save each image URL as a separate entry in the product_images table
                 foreach ($imagesData as $imageUrl) {
                     $this->productImagesRepository->create([
                         'product_id' => $product->id,
@@ -73,16 +85,23 @@ class APIController extends Controller
         }
     }
 
-
-    // The getAllProducts function is responsible for getting all the products from our database
-    // and then returning the data in the format the client accepts (JSON or XML)
+    /*
+     * The getAllProducts method is responsible for retrieving all stored products and returning them in a client-accepted format.
+     * The format can be JSON or XML.
+     * If no specific format is requested, the default format is JSON.
+     *
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\Response - The response containing the products data in the requested format.
+     */
     public function getAllProducts()
     {
+        // Fetch all products.
         $products = $this->productRepository->getAll();
 
+        // Determine the response format and return the data.
         if (request()->wantsJson() || request()->accepts('application/json')) {
             return response()->json($products);
         } elseif (request()->accepts('text/xml') || request()->accepts('application/xml')) {
+            // Build XML and return it.
             $xml = new \SimpleXMLElement('<root/>');
             foreach ($products->toArray() as $item) {
                 $product = $xml->addChild('product');
@@ -100,7 +119,7 @@ class APIController extends Controller
             return response($xml->asXML(), 200, ['Content-Type' => 'application/xml']);
         }
 
-        // Default response format is JSON
+        // Default response format is JSON.
         return response()->json($products);
     }
 }
--- app/Services/XmlParser.php
+++ app/Services/XmlParser.php
@@ -1,32 +1,36 @@
 <?php
 
-namespace App\Services; // Define the namespace for this class.
+// Define the namespace for the class.
+namespace App\Services;
 
+// Import the ParserInterface which this class implements.
 use App\Repositories\Interfaces\ParserInterface;
-
-// Import the ParserInterface.
-//This is needed because the ParserInterface is not in the same namespace as this class.
 
 /*
  * XmlParser is a class that implements the ParserInterface.
- * This means that this class must contain all methods defined in ParserInterface.
- * In this case, it means that the XmlParser class must have a parse method.
+ * The class is responsible for parsing XML data and converting it into a PHP array.
  */
 class XmlParser implements ParserInterface
 {
     /*
-     * The parse method is required by the ParserInterface.
-     * It takes in a string of data and attempts to parse it as XML.
-     * @param string $data - The data to be parsed.
-     * @return \SimpleXMLElement - The parsed data, as a SimpleXMLElement object.
+     * The parse method takes a string of XML data and converts it into a PHP associative array.
+     * If the 'products' key in the XML data is not an array, it converts it into an array. This ensures consistent data structure.
+     * If the 'images' key in each product is not an array, it converts it into an array. This ensures that each product can have multiple images.
+     * @param string $xml - The XML data to be parsed.
+     * @return array - The parsed data, as a PHP associative array.
      */
     public function parse($xml): array
     {
+        // Load the XML string as a SimpleXMLElement object.
         $xmlObject = simplexml_load_string($xml, "SimpleXMLElement", LIBXML_NOCDATA);
+        
+        // Convert the SimpleXMLElement object to JSON.
         $json = json_encode($xmlObject);
+        
+        // Decode the JSON to a PHP array.
         $phpArray = json_decode($json, true);
 
-        // Adjust the 'products' array to ensure each product is an array item
+        // Ensure that the 'products' key is an array.
         $products = [];
         foreach ($phpArray['products'] as $product) {
             // If 'images' is not an array (only one image), make it an array
@@ -36,12 +40,10 @@ class XmlParser implements ParserInterface
             $products[] = $product;
         }
 
-        // Replace the 'products' item with our new products array
+        // Replace the 'products' key with the new array.
         $phpArray['products'] = $products;
-        //dd($phpArray);
+
+        // Return the PHP array.
         return $phpArray;
     }
-
-
 }
-
